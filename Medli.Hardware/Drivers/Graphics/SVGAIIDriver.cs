855f51af990db7b60585a1d99cae615b16eb7d34
Siaranite <arawn.780@outlook.com>
2018-04-04 10:08:25 +0100

    HAL Driver restructure

---
+++ Medli.Hardware/Drivers/Graphics/SVGAIIDriver.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medli.Hardware.Drivers.Graphics
+{
+    class SVGAIIDriver
+    {
+    }
+}
---
+++ Medli.Hardware/Drivers/Graphics/VBEDriver.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medli.Hardware.Drivers
+{
+    class VBEDriver
+    {
+    }
+}
