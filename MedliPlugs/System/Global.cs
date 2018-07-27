bf2c32f34507424b9b39026f8429ef72ab548a49
Arawn Davies <arawn.davies780@gmail.com>
2018-07-28 00:13:35 +0100

    Updated plugs

---
+++ MedliPlugs/System/Global.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+using Cosmos.HAL;
+using IL2CPU.API.Attribs;
+
+namespace MedliPlugs.System
+{
+	[Plug(Target = typeof(Cosmos.System.Global))]
+    public static class Global
+    {
+		public static void Init(TextScreenBase textScreen)
+		{
+			HAL.Global.Init(textScreen);
+			Sys.Global.NumLock = false;
+			Sys.Global.CapsLock = false;
+			Sys.Global.ScrollLock = false;
+		}
+    }
+}
