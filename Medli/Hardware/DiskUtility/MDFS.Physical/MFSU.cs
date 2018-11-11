064875f4e7807feb0ad457460c970b482dada401
Arawn Davies <arawn.davies@gmail.com>
2018-11-11 23:25:31 +0000

    Add annotation

--- Medli/Hardware/DiskUtility/MDFS.Physical/MFSU.cs
+++ Medli/Hardware/DiskUtility/MDFS.Physical/MFSU.cs
@@ -1,62 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using MDFS.Physical;
+using Cosmos.HAL;
 
 namespace MDFS
 {
-	/// <summary>
-	/// Defines a disk listing
-	/// </summary>
-	public class DiskListing
-	{
-		/// <summary>
-		/// Static List of DiskListings
-		/// </summary>
-		public static List<DiskListing> DiskListings = new List<DiskListing>();
-        
+    /// <summary>
+    /// Defines a disk listing
+    /// </summary>
+    public class DiskListing
+    {
+        /// <summary>
+        /// Static List of DiskListings
+        /// </summary>
+        public static List<DiskListing> DiskListings = new List<DiskListing>();
+
         /// <summary>
         /// Static List of PartitionListings
         /// </summary>
         public List<PartitionListing> PartitionListings = new List<PartitionListing>();
 
-		/// <summary>
-		/// The DiskListing's BlockDevice
-		/// </summary>
-		public IDE Disk;
+        /// <summary>
+        /// The DiskListing's BlockDevice
+        /// </summary>
+        public IDE Disk;
 
-		/// <summary>
-		/// The DiskListing's Disk Number
-		/// </summary>
-		public int DiskNumber;
+        /// <summary>
+        /// The DiskListing's Disk Number
+        /// </summary>
+        public int DiskNumber;
 
-		/// <summary>
-		/// Constructor for a Disk Listing
-		/// </summary>
-		/// <param name="num"></param>
-		/// <param name="disk"></param>
-		public DiskListing(int num, IDE disk)
-		{
-			DiskNumber = num + 1;
-			Disk = disk;
-			DiskListings.Add(this);
+        /// <summary>
+        /// Constructor for a Disk Listing
+        /// </summary>
+        /// <param name="num"></param>
+        /// <param name="disk"></param>
+        public DiskListing(int num, IDE disk)
+        {
+            DiskNumber = num + 1;
+            Disk = disk;
+            DiskListings.Add(this);
             int pnum = 0;
             foreach (PrimaryPartition Partition in disk.PrimaryPartitions)
             {
                 pnum += 1;
                 PartitionListings.Add(new PartitionListing(pnum, this, Partition.Infos));
             }
-		}
+        }
 
-		/// <summary>
-		/// String containing information about the Disk Listing
-		/// </summary>
-		public string Info
-		{
-			get
-			{
-				return ("Disk: " + DiskNumber + " Size: " + Disk.Size + " MBs");
-			}
-		}
+        /// <summary>
+        /// String containing information about the Disk Listing
+        /// </summary>
+        public string Info
+        {
+            get
+            {
+                return ("Disk: " + DiskNumber + " Size: " + Disk.Size + " MBs");
+            }
+        }
 
         public void PartInfo()
         {
@@ -66,15 +67,18 @@ namespace MDFS
             }
         }
 
-		/// <summary>
-		/// Prints information about the DiskListing
-		/// </summary>
-		public void PrintInfo()
-		{
-			Console.WriteLine(Info);
-		}
-	}
+        /// <summary>
+        /// Prints information about the DiskListing
+        /// </summary>
+        public void PrintInfo()
+        {
+            Console.WriteLine(Info);
+        }
+    }
 
+    /// <summary>
+    /// Defines a partition listing
+    /// </summary>
     public class PartitionListing
     {
         public DiskListing Disk;
@@ -101,30 +105,30 @@ namespace MDFS
         }
     }
 
-	/// <summary>
-	/// The Disk Utility class, containing methods and properties
-	/// </summary>
-	public class MFSUtility
-	{
-		/// <summary>
-		/// Prints information about the currently selected disk, if any is selected
-		/// </summary>
-		public static void PrintSelectedDisk()
-		{
-			if (SelectedDisk != null)
-			{
+    /// <summary>
+    /// The Disk Utility class, containing methods and properties
+    /// </summary>
+    public class MFSUtility
+    {
+        /// <summary>
+        /// Prints information about the currently selected disk, if any is selected
+        /// </summary>
+        public static void PrintSelectedDisk()
+        {
+            if (SelectedDisk != null)
+            {
 
-				SelectedDisk.PrintInfo();
-			}
-			else
-			{
-				Console.WriteLine("No disk is currently selected!");
-			}
-		}
-		/// <summary>
-		/// Defines the selected DiskListing
-		/// </summary>
-		public static DiskListing SelectedDisk = null;
+                SelectedDisk.PrintInfo();
+            }
+            else
+            {
+                Console.WriteLine("No disk is currently selected!");
+            }
+        }
+        /// <summary>
+        /// Defines the selected DiskListing
+        /// </summary>
+        public static DiskListing SelectedDisk = null;
 
         /// <summary>
         /// Main Disk Utility method, with the list of IDE devices passed as the parameter
@@ -132,89 +136,89 @@ namespace MDFS
         /// </summary>
         /// <param name="List"></param>
         public static void Main(IDE[] List)
-		{
-            
-			bool running = true;
-			Console.BackgroundColor = ConsoleColor.Blue;
-			Console.Clear();
-			Console.WriteLine("Welcome to the Medli File System Utility [MFSU]");
-			Console.WriteLine("");
-			while (running == true)
-			{
-				Console.WriteLine("");
-				Console.WriteLine("1) List Disks 2) Select Disks 3) List Partitions\n4) Create Partitions 5) Exit disk utility");
+        {
+
+            bool running = true;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("Welcome to the Medli File System Utility [MFSU]");
+            Console.WriteLine("");
+            while (running == true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("1) List Disks 2) Select Disks 3) List Partitions\n4) Create Partitions 5) Exit disk utility");
                 Console.WriteLine("");
                 Console.Write(">");
                 string option = Console.ReadLine().ToLower();
-				if (option == "1")
-				{
-					ListDisks();
-				}
-				else if (option == "2")
-				{
-					SelectDisk();
-				}
-				else if (option == "3")
-				{
+                if (option == "1")
+                {
+                    ListDisks();
+                }
+                else if (option == "2")
+                {
+                    SelectDisk();
+                }
+                else if (option == "3")
+                {
                     DiskPartInfo();
-				}
-				else if (option == "4")
-				{
-					CreatePartitions();
-				}
-				else if (option == "quit" || option == "exit" || option == "5")
-				{
-					running = false;
-				}
-				else
-				{
-					Console.WriteLine(option + ": Invalid option!");
-				}
-			}
-			Console.Clear();
-		}
-		/// <summary>
-		/// Selects a DiskListing from the list
-		/// </summary>
-		public static void SelectDisk()
-		{
-			SelectedDisk = null;
-			do
-			{
-				Console.WriteLine("Which disk do you wish to use?\n");
-				ListDisks();
-				string nums = Console.ReadLine();
-				int num = int.Parse(nums) - 1;
-				if (num >= 0 && num < DiskListing.DiskListings.Count)
-				{
-					SelectedDisk = DiskListing.DiskListings[num];
-				}
-			}
-			while (SelectedDisk == null);
-		}
+                }
+                else if (option == "4")
+                {
+                    CreatePartitions();
+                }
+                else if (option == "quit" || option == "exit" || option == "5")
+                {
+                    running = false;
+                }
+                else
+                {
+                    Console.WriteLine(option + ": Invalid option!");
+                }
+            }
+            Console.Clear();
+        }
+        /// <summary>
+        /// Selects a DiskListing from the list
+        /// </summary>
+        public static void SelectDisk()
+        {
+            SelectedDisk = null;
+            do
+            {
+                Console.WriteLine("Which disk do you wish to use?\n");
+                ListDisks();
+                string nums = Console.ReadLine();
+                int num = int.Parse(nums) - 1;
+                if (num >= 0 && num < DiskListing.DiskListings.Count)
+                {
+                    SelectedDisk = DiskListing.DiskListings[num];
+                }
+            }
+            while (SelectedDisk == null);
+        }
 
-		/// <summary>
-		/// Lists each of the disks from the list
-		/// </summary>
-		public static void ListDisks()
-		{
-			foreach (DiskListing disklist in DiskListing.DiskListings)
-			{
-				if (disklist == SelectedDisk)
-				{
-					Console.WriteLine("* " + disklist.Info);
-				}
-				else
-				{
-					disklist.PrintInfo();
-				}
-			}
-		}
+        /// <summary>
+        /// Lists each of the disks from the list
+        /// </summary>
+        public static void ListDisks()
+        {
+            foreach (DiskListing disklist in DiskListing.DiskListings)
+            {
+                if (disklist == SelectedDisk)
+                {
+                    Console.WriteLine("* " + disklist.Info);
+                }
+                else
+                {
+                    disklist.PrintInfo();
+                }
+            }
+        }
 
-		/// <summary>
-		/// Lists the partitions in the specified disk
-		/// </summary>
-		/// <param name="Device"></param>
+        /// <summary>
+        /// Lists the partitions in the specified disk
+        /// </summary>
+        /// <param name="Device"></param>
         public static void DiskPartInfo()
         {
             if (SelectedDisk != null)
@@ -256,66 +260,66 @@ namespace MDFS
         /// Creates partitions on the selected disk
         /// </summary>
         public static void CreatePartitions()
-		{
-			if (SelectedDisk == null)
-			{
-				Console.WriteLine("No disk is currently selected!");
-			}
-			else
-			{
-				int PartitionNumber = 0;
-				ulong DisplayCount = SelectedDisk.Disk.BlockCount - 1;
-				Console.WriteLine("How many primary partitions do you want to have? (Max. 4)");
-				do
-				{
-					Console.WriteLine("Insert number");
-					String nums = Console.ReadLine();
-					PartitionNumber = int.Parse(nums);
-				}
-				while (PartitionNumber == 0 || PartitionNumber > 4);
+        {
+            if (SelectedDisk == null)
+            {
+                Console.WriteLine("No disk is currently selected!");
+            }
+            else
+            {
+                int PartitionNumber = 0;
+                ulong DisplayCount = SelectedDisk.Disk.BlockCount - 1;
+                Console.WriteLine("How many primary partitions do you want to have? (Max. 4)");
+                do
+                {
+                    Console.WriteLine("Insert number");
+                    String nums = Console.ReadLine();
+                    PartitionNumber = int.Parse(nums);
+                }
+                while (PartitionNumber == 0 || PartitionNumber > 4);
 
-				uint mbrpos = 446;
-				Byte[] type = new Byte[] { 0x00, 0x00, 0x00, 0x00 };
-				uint[] StartBlock = new uint[] { 1, 0, 0, 0 };
-				uint[] BlockNum = new uint[] { 0, 0, 0, 0 };
-				for (int i = 0; i < PartitionNumber; i++)
-				{
-					type[i] = 0xFA;
-					Console.WriteLine("How many blocks for Partition N. " + (i + 1) + "? (Max: " + ((uint)(DisplayCount - (uint)(PartitionNumber - (i + 1)))).ToString() + "): ");
-					String nums = Console.ReadLine();
-					uint num = (uint)int.Parse(nums);
-					if (num >= 0 && num <= DisplayCount - (uint)(PartitionNumber - (i + 1)))
-					{
-						BlockNum[i] = num;
-						if (i < PartitionNumber - 1)
-						{
-							StartBlock[i + 1] = (num) + StartBlock[i];
-						}
-						DisplayCount = DisplayCount - (num);
-					}
-					else
-					{
-						i--;
-					}
-				}
-				Byte[] data = SelectedDisk.Disk.NewBlockArray(1);
-				SelectedDisk.Disk.ReadBlock(0, 1, data);
-				for (int i = 0; i < 4; i++)
-				{
-					mbrpos += 4;
-					data[mbrpos] = type[i];
-					mbrpos += 4;
-					Byte[] b = BitConverter.GetBytes(StartBlock[i]);
-					Utilities.CopyByteToByte(b, 0, data, (int)mbrpos, b.Length);
-					mbrpos += 4;
-					b = BitConverter.GetBytes(BlockNum[i]);
-					Utilities.CopyByteToByte(b, 0, data, (int)mbrpos, b.Length);
-					mbrpos += 4;
-					SelectedDisk.Disk.WriteBlock(StartBlock[i], 1, SelectedDisk.Disk.NewBlockArray(1));
-				}
-				SelectedDisk.Disk.WriteBlock(0, 1, data);
+                uint mbrpos = 446;
+                Byte[] type = new Byte[] { 0x00, 0x00, 0x00, 0x00 };
+                uint[] StartBlock = new uint[] { 1, 0, 0, 0 };
+                uint[] BlockNum = new uint[] { 0, 0, 0, 0 };
+                for (int i = 0; i < PartitionNumber; i++)
+                {
+                    type[i] = 0xFA;
+                    Console.WriteLine("How many blocks for Partition N. " + (i + 1) + "? (Max: " + ((uint)(DisplayCount - (uint)(PartitionNumber - (i + 1)))).ToString() + "): ");
+                    String nums = Console.ReadLine();
+                    uint num = (uint)int.Parse(nums);
+                    if (num >= 0 && num <= DisplayCount - (uint)(PartitionNumber - (i + 1)))
+                    {
+                        BlockNum[i] = num;
+                        if (i < PartitionNumber - 1)
+                        {
+                            StartBlock[i + 1] = (num) + StartBlock[i];
+                        }
+                        DisplayCount = DisplayCount - (num);
+                    }
+                    else
+                    {
+                        i--;
+                    }
+                }
+                Byte[] data = SelectedDisk.Disk.NewBlockArray(1);
+                SelectedDisk.Disk.ReadBlock(0, 1, data);
+                for (int i = 0; i < 4; i++)
+                {
+                    mbrpos += 4;
+                    data[mbrpos] = type[i];
+                    mbrpos += 4;
+                    Byte[] b = BitConverter.GetBytes(StartBlock[i]);
+                    Utilities.CopyByteToByte(b, 0, data, (int)mbrpos, b.Length);
+                    mbrpos += 4;
+                    b = BitConverter.GetBytes(BlockNum[i]);
+                    Utilities.CopyByteToByte(b, 0, data, (int)mbrpos, b.Length);
+                    mbrpos += 4;
+                    SelectedDisk.Disk.WriteBlock(StartBlock[i], 1, SelectedDisk.Disk.NewBlockArray(1));
+                }
+                SelectedDisk.Disk.WriteBlock(0, 1, data);
                 DiskPartInfo();
-			}
-		}
-	}
+            }
+        }
+    }
 }
