4459f0b8582f88f9354b92bc57fa3ed6f90922c2
Arawn Davies <arawn.davies@gmail.com>
2018-11-13 00:41:56 +0000

    Initial definitions

--- Medli/System/MDFS/Block.cs
+++ Medli/System/MDFS/Block.cs
@@ -7,7 +7,205 @@ using Cosmos.HAL.BlockDevice;
 
 namespace Medli.System.MDFS
 {
-    class MDBlock
+    public class MDBlock
     {
+        public static uint MaxBlockContentSize = 491;
+
+        private Partition _partition;
+
+        private ulong _blockNo = 0;
+
+        private bool _Used = false;
+        private uint _cSize = 0;
+        private ulong _tSize = 0;
+        private ulong _nBlock = 0;
+
+        /// <summary>
+        /// The partition holding the block
+        /// </summary>
+        public Partition Partition
+        {
+            get
+            {
+                return _partition;
+            }
+        }
+
+        /// <summary>
+        /// The number of the current block
+        /// </summary>
+        public ulong BlockNumber
+        {
+            get
+            {
+                return _blockNo;
+            }
+        }
+
+        /// <summary>
+        /// Bool whether block used or not used
+        /// </summary>
+        public bool Used
+        {
+            get
+            {
+                return _Used;
+            }
+            set
+            {
+                _Used = value;
+            }
+        }
+
+        /// <summary>
+        /// The current content Size
+        /// </summary>
+        public uint ContentSize
+        {
+            get
+            {
+                return _cSize;
+            }
+            set
+            {
+                _cSize = value;
+            }
+        }
+
+        /// <summary>
+        /// Total size of the Entry (Not Used)
+        /// </summary>
+        public ulong TotalSize
+        {
+            get
+            {
+                return _tSize;
+            }
+            set
+            {
+                _tSize = value;
+            }
+        }
+
+        /// <summary>
+        /// The next block number to read al the content
+        /// </summary>
+        public ulong NextBlock
+        {
+            get
+            {
+                return _nBlock;
+            }
+            set
+            {
+                _nBlock = value;
+            }
+        }
+
+        /// <summary>
+        /// The Content Byte Array. TO-DO: code this better
+        /// </summary>
+        public Byte[] Content;
+
+        /// <summary>
+        /// Creates a new virtual Block.
+        /// </summary>
+        /// <param name="Data">The Byte data</param>
+        /// <param name="p">The partition to use</param>
+        /// <param name="bn">The block number</param>
+        public MDBlock(Byte[] Data, Partition p, ulong bn)
+        {
+            _blockNo = bn;
+            _partition = p;
+            Content = new Byte[Data.Length - 21];
+            if (Data[0] == 0x00)
+            {
+                _Used = false;
+                for (int i = 0; i < Content.Length; i++)
+                {
+                    Content[i] = 0;
+                }
+            }
+            else
+            {
+                _Used = true;
+                _cSize = BitConverter.ToUInt32(Data, 1);
+                _tSize = BitConverter.ToUInt64(Data, 5);
+                _nBlock = BitConverter.ToUInt64(Data, 13);
+                for (int i = 21; i < Data.Length; i++)
+                {
+                    Content[i - 21] = Data[i];
+                }
+            }
+        }
+
+        /// <summary>
+        /// The
+        /// </summary>
+        /// <param name="p">The partition to read</param>
+        /// <param name="bn">The blocknumber to read</param>
+        public static MDBlock Read(Partition p, ulong bn)
+        {
+            Byte[] data = p.NewBlockArray(1);
+            p.ReadBlock(bn, 1, data);
+            return new MDBlock(data, p, bn);
+        }
+
+        /// <summary>
+        /// Writes a block into a specific partition
+        /// </summary>
+        /// <param name="p">The partition to write to</param>
+        /// <param name="b">The block to write</param>
+        public static void Write(Partition p, MDBlock b)
+        {
+            Byte[] data = new Byte[p.BlockSize];
+            int index = 0;
+            if (b.Used)
+            {
+                data[index++] = 0x01;
+            }
+            else
+            {
+                data[index++] = 0x00;
+            }
+            Byte[] x = BitConverter.GetBytes(b.ContentSize);
+            for (int i = 0; i < x.Length; i++)
+            {
+                data[index++] = x[i];
+            }
+            x = BitConverter.GetBytes(b.TotalSize);
+            for (int i = 0; i < x.Length; i++)
+            {
+                data[index++] = x[i];
+            }
+            x = BitConverter.GetBytes(b.NextBlock);
+            for (int i = 0; i < x.Length; i++)
+            {
+                data[index++] = x[i];
+            }
+            x = b.Content;
+            for (int i = 0; i < x.Length; i++)
+            {
+                data[index++] = x[i];
+            }
+            p.WriteBlock(b.BlockNumber, 1, data);
+        }
+
+        /// <summary>
+        /// Get the next free block from the selected partition (TO-DO: Implement something that runs faster)
+        /// </summary>
+        /// <param name="p">The partition to get the block from</param>
+        public static MDBlock GetFreeBlock(Partition p)
+        {
+            for (ulong i = 1; i < p.BlockCount; i++)
+            {
+                MDBlock b = Read(p, i);
+                if (!b.Used)
+                {
+                    return b;
+                }
+            }
+            return null;
+        }
     }
 }
--- Medli/System/MDFS/Entry.cs
+++ Medli/System/MDFS/Entry.cs
@@ -7,7 +7,133 @@ using Cosmos.HAL.BlockDevice;
 
 namespace Medli.System.MDFS
 {
-    class MDEntry
+    /// <summary>
+    /// Modifiable attributes of filesystem entries
+    /// </summary>
+    public enum EntryAttribute
     {
+        /// <summary>
+        /// Date/Time Created
+        /// </summary>
+        DtC = 0x00,
+
+        /// <summary>
+        /// Date/Time Modified
+        /// </summary>
+        DtM = 0x08,
+
+        /// <summary>
+        /// Date/Time Last Accessed
+        /// </summary>
+        DtA = 0x10,
+
+        /// <summary>
+        /// Owner
+        /// </summary>
+        OwR = 0x18,
+
+        /// <summary>
+        /// Group
+        /// </summary>
+        GoP = 0x20,
+
+        /// <summary>
+        /// Visisibility
+        /// </summary>
+        HdN = 0x28,
+
+        /// <summary>
+        /// Read/Write Permissions
+        /// </summary>
+        ReW = 0x29,
+
+        /// <summary>
+        /// Owner Permissions
+        /// </summary>
+        OwP = 0x30,
+
+        /// <summary>
+        /// Group Permissions
+        /// </summary>
+        GrP = 0x3a,
+
+        /// <summary>
+        /// Global Permissions
+        /// </summary>
+        GlP = 0x3b
+    }
+
+    public class MDEntry
+    {
+        /// <summary>
+        /// Char[] array of characters that are invalid for use in files/directories
+        /// </summary>
+        protected static Char[] UnacceptableChars = new char[] { '/', '?', '*' };
+
+        /// <summary>
+        /// The starting block of this FileSystem entry
+        /// </summary>
+        protected MDBlock sBlock;
+
+        /// <summary>
+        /// The filesystem path of the entry
+        /// </summary>
+        protected String _path;
+
+        /// <summary>
+        /// The partition upon which the entry resides
+        /// </summary>
+        protected Partition _partition;
+
+        /// <summary>
+        /// The maximum length a filename can be
+        /// </summary>
+        private static int MaxFNL = 255;
+
+        public String Path
+        {
+            get
+            {
+                return _path;
+            }
+        }
+
+        /// <summary>
+        /// Returns the FileSystem entryname
+        /// </summary>
+        public String Name
+        {
+            get
+            {
+                Byte[] arr = sBlock.Content;
+                String ret = "";
+                for (int i = 0; i < MaxFNL; i++)
+                {
+                    if (arr[i] == 0)
+                    {
+                        break;
+                    }
+                    ret += ((Char) arr[i]).ToString();
+                }
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Sets the entry's attributes to the specified
+        /// </summary>
+        /// <param name="Entry"></param>
+        /// <param name="value"></param>
+        public void EditAttributes(EntryAttribute Attrib, long value)
+        {
+            if (Attrib < EntryAttribute.HdN)
+            {
+                Utilities.CopyByteToByte(BitConverter.GetBytes(value), 0, sBlock.Content, MaxFNL + (int)Attrib, 8, false);
+            }
+            else
+            {
+                Utilities.CopyByteToByte(BitConverter.GetBytes(value), 0, sBlock.Content, MaxFNL + (int)Attrib, 1, false);
+            }
+        }
     }
 }
