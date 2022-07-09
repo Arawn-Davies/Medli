b6c13a095db944e6a07e5de241f222ae906d5c02
Arawn Davies <arawn.davies@gmail.com>
2022-07-09 15:20:34 +0100

    Updated AIC

--- Medli/Common/VersionInfo.cs
+++ Medli/Common/VersionInfo.cs
@@ -8,6 +8,6 @@ namespace Medli
 		///<summary>
 		///The kernel build number, generated upon a new build
 		///</summary>
-        public static string BuildNumber = "090720220028";
+        public static string BuildNumber = "090720221423";
     }
 }
